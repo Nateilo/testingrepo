883e4f721962ce47be56a1ccccf931ae1329e379
DESKTOP-BUQ048Q\Nate <nathe@live.com>
2016-08-20 01:26:20 +0100

    Initial commit

---
+++ MyProject/LoginSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MyProject
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"I have navigated to the ASOS login page")]
+        public void GivenIHaveNavigatedToTheASOSLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I provide valid login details")]
+        public void GivenIProvideValidLoginDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I successfully login to the website")]
+        public void ThenISuccessfullyLoginToTheWebsite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
