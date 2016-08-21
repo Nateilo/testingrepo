4c42a6650ebff50877fdee384acb3ba0aa4573b3
DESKTOP-BUQ048Q\Nate <nathe@live.com>
2016-08-21 11:47:06 +0100

    Initial commit

---
+++ MyTestProject/UnitTestProject/Login.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.PhantomJS;
+using System;
+
+namespace UnitTestProject
+{
+    [Binding]
+    public class Login
+    {
+        private string baseURL = "https://www.asos.com/";
+        private RemoteWebDriver driver;
+        private string browser;
+        [Given(@"I have opened the ASOS website")]
+        public void GivenIHaveOpenedTheASOSWebsite()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElementById("search - box").Clear();
+            driver.FindElementById("search - box").SendKeys("tire");
+        }
+        
+        [Given(@"I am on the homepage")]
+        public void GivenIAmOnTheHomepage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I tap the login button")]
+        public void WhenITapTheLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I provide valid login details")]
+        public void WhenIProvideValidLoginDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am logged in")]
+        public void ThenIAmLoggedIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
+
---
+++ MyTestProject/UnitTestProject/SpecFlowFeature1.feature
@@ -0,0 +1,12 @@
+﻿Feature: Login details
+	In order to verify login details work
+	As a user
+	I want to be able to login to the ASOS website
+
+@mytag
+Scenario: Login
+	Given I have opened the ASOS website
+	And I am on the homepage
+	When I tap the login button
+	And I provide valid login details
+	Then I am logged in
